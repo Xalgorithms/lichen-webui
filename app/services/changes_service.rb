04e4ac179c455cd688d38e6540be0411ef7da3cc
Don Kelly <karfai@gmail.com>
2017-05-14 03:42:39 -0400

    [pick] destroyed in changes service

--- app/services/changes_service.rb
+++ app/services/changes_service.rb
@@ -1,6 +1,16 @@
 class ChangesService
   def self.added(m)
-    payload = { id: m.public_id, effect: 'added', model: m.class.name.demodulize.underscore  }
-    ActionCable.server.broadcast('changes', payload)    
+    send('added', m)
+  end
+
+  def self.destroyed(m)
+    send('destroyed', m)
+  end
+
+  private
+
+  def self.send(effect, m)
+    payload = { id: m.public_id, effect: effect, model: m.class.name.demodulize.underscore  }
+    ActionCable.server.broadcast('changes', payload)        
   end
 end
